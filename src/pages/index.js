d9eaab44da500c51f26ec11d5bd2a5acc193194f
zachlegros <legroszach3921@gmail.com>
2021-05-18 19:00:53 -0400

    Styled survey components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,63 @@
 import * as React from "react"
-// import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Container from "../components/container"
 import Seo from "../components/seo"
 
+import "../components/survey.css"
+
 const IndexPage = () => (
   <Layout>
     <Seo title="UI Survey" />
     <Container>
-      <h1>UI Survey</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <StaticImage
-        src="../images/gatsby-astronaut.png"
-        width={300}
-        quality={95}
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt="A Gatsby astronaut"
-        style={{ marginBottom: `1.45rem` }}
-      />
-      <p>{/* <Link to="/page-2/">Go to page 2</Link> <br /> */}</p>
-      <input type="text" placeholder="Full name" />
-      <br />
+      <h1>Twitter UI Survey</h1>
+      <p>
+        We would like to have your thoughts on the{" "}
+        <a href="https://twitter.com/home" target="_blank">
+          Twitter
+        </a>{" "}
+        homepage user interface.
+      </p>
+      <a
+        href="https://twitter.com/home"
+        target="_blank"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+          borderRadius: "0.5rem",
+          marginBottom: "2.5rem",
+        }}
+      >
+        <StaticImage
+          src="../images/twitter.png"
+          width={700}
+          quality={100}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          className="website-preview"
+          style={{
+            height: "100%",
+            transition: "opacity .25s linear, transform 0.2s",
+          }}
+        />
+      </a>
+
+      <div className="input-section">
+        <h2>Tell us a little bit about you</h2>
+        <div className="input-group">
+          <p className="input-title">First name</p>
+          <input type="text" placeholder="Enter your first name" />
+        </div>
+        <div className="input-group">
+          <p className="input-title">Last name</p>
+          <input type="text" placeholder="Enter your Last name" />
+        </div>
+      </div>
+      <div className="input-section">
+        <h2>What do you like?</h2>
+      </div>
       <input id="r1" type="radio" name="radio" value="1" />
       <label for="r1">Radio</label>
       <input id="c1" type="checkbox" name="checkbox" value="1" />
